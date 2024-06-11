7d1235d00c6c5bda42e5a71893ac08050c449e37
dariosalascardyn <167145326+dariosalascardyn@users.noreply.github.com>
2024-06-10 21:05:34 -0400

    Update funcionesactividad.py

--- funcionesactividad.py
+++ funcionesactividad.py
@@ -24,4 +24,15 @@ def cuadrado_de_numeros(listacuadrados):
         return numerocuadrado
     
 # funcion cantidad de  palabraas
+def cantidad_palabras (palabras):
+    numeropalabras = len(palabras.split())
+    print(f"el numero de palabras es: {numeropalabras}")
+    return(numeropalabras)
 
+#funcion cantidad de letras
+def cantidad_letras (letras):
+    letras_sin_espacio = letras.split()
+    letras_sin_espacio= "".join(letras_sin_espacio)
+    numeroletras = len(letras_sin_espacio)
+    print(f"el numero de letras es: {numeroletras}")
+    return(numeroletras)
